a7e1a4e7d8f22f60dff1de46ea0daf0687e8e49b
Gibran Castillo <gibran-castillo@hotmail.com>
2022-03-09 19:19:21 -0600

    Learning Web Design and Development

---
+++ w02/asgmt2/js/last_modified.js
@@ -0,0 +1,6 @@
+try {
+  //document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()));
+  document.getElementById('last_modified').textContent = "Last Updated: " + document.lastModified;
+} catch (e) {
+  alert("Error with code or your browser does not support locale");
+}
