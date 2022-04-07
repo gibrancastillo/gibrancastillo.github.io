0cf13b5cd541546599e0391c0c1c9651f9bf5cd0
Gibran Castillo <gibran-castillo@hotmail.com>
2022-04-07 17:19:10 -0500

    Complete rental webpage

--- scooting/js/rentals_api.js
+++ scooting/js/rentals_api.js
@@ -1,9 +1,11 @@
-const rentalsApiUrl = "../assets/data/data.json";
 const grid = document.querySelector("#grid");
 const list = document.querySelector("#list");
 const rental = document.querySelector("#rental");
 
-fetch(rentalsApiUrl)
+const rentalsApiUrl = "../assets/data/data.json";
+const rentalsApiMockUrl = "https://run.mocky.io/v3/c7c88af0-3856-4253-aa7d-3a5c7f795e1c";
+
+fetch(rentalsApiMockUrl)
   .then((response) => response.json())
   .then((jsonObject) => {
       console.table(jsonObject);
@@ -26,6 +28,7 @@ function displayCards(card) {
   // Create elements to add to the document
   let card_section = document.createElement('section');
   let h2 = document.createElement('h2');
+  let p = document.createElement('p');
   let p1 = document.createElement('p');
   let p2 = document.createElement('p');
   let p3 = document.createElement('p');
@@ -35,11 +38,11 @@ function displayCards(card) {
   newDiv.className = "logo-company";
 
   h2.textContent = `${card.rental_type}`
-  p1.innerHTML = `<strong>Rental Location: </strong> ${card.rental_location}`;
-  p1.innerHTML = `<strong>Address: </strong> ${card.address}`;
-  p2.innerHTML = `<strong>Phone number: </strong> ${card.phone_number}`;
-  p3.innerHTML = `<strong>Reservation: </strong> ${card.reservation}`;
-  p4.innerHTML = `<strong>Walk-In: </strong> ${card.walk_in}`;
+  p.innerHTML = `<span style="font-weight:bold;">Rental Location: </span> ${card.rental_location}`;
+  p1.innerHTML = `${card.address}`;
+  p2.innerHTML = `${card.phone_number}`;
+  p3.innerHTML = `<span style="font-weight:bold;">Reservation: </span> ${card.reservation}`;
+  p4.innerHTML = `<span style="font-weight:bold;">Walk-In: </span> ${card.walk_in}`;
 
   // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
   img.setAttribute('src', card.image);
@@ -50,10 +53,11 @@ function displayCards(card) {
   newDiv.appendChild(img)
   card_section.appendChild(newDiv);
   card_section.appendChild(h2);
-  card_section.appendChild(p1)
-  card_section.appendChild(p2)
-  card_section.appendChild(p3)
-  card_section.appendChild(p4)
+  card_section.appendChild(p);
+  card_section.appendChild(p1);
+  card_section.appendChild(p2);
+  card_section.appendChild(p3);
+  card_section.appendChild(p4);
 
 
   // Add/append the existing HTML div with the cards class with the section(card)
