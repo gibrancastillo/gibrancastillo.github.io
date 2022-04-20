a300376aa3aad50013abd884d1543dfe73897332
Gibran Castillo <gibran-castillo@hotmail.com>
2022-04-19 22:13:17 -0500

    Add WDD 330 Assignment Portal

---
+++ js/last_modified.js
@@ -0,0 +1,2 @@
+document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()));
+document.getElementById('last_modified').textContent = "Last Updated: " + document.lastModified;
--- scooting/js/toggle_menu.js
+++
@@ -1,5 +0,0 @@
-function toggleMenu() {
-    document.querySelector("#navigation").classList.toggle("open");
-}
-
-document.querySelector("#hamburger-btn").onclick = toggleMenu;
